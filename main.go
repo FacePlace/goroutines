bc02e3a2457782aa92cbff14ee0b36e0e23b95e2
ZeroWorldwide <43105109+FacePlace@users.noreply.github.com>
2022-04-14 19:10:07 +0300

    Sync

--- main.go
+++ main.go
@@ -2,15 +2,34 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"runtime"
+	"sync"
 )
 
+var wg = sync.WaitGroup{}
+var counter = 0
+var m = sync.RWMutex{}
+
 func main() {
-	msg := "stuff"
+	runtime.GOMAXPROCS(100)
+	for i := 0; i < 10; i++ {
+		wg.Add(2)
+		m.RLock()
+		go sayHello()
+		m.Lock()
+		go increment()
+	}
+	wg.Wait()
+}
 
-	go func(msg string) {
-		fmt.Println(msg)
-	}(msg)
+func sayHello() {
+	fmt.Printf("Hello #%v\n", counter)
+	m.RUnlock()
+	wg.Done()
+}
 
-	time.Sleep(100 * time.Microsecond)
+func increment() {
+	counter++
+	m.Unlock()
+	wg.Done()
 }
